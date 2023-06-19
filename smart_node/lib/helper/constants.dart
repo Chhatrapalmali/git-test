4461327eb0d88421e282fe6fcaadb6822432b147
Chhatrapalmali <125431784+Chhatrapalmali@users.noreply.github.com>
2023-06-19 10:00:54 +0530

    Update constants.dart

--- smart_node/lib/helper/constants.dart
+++ smart_node/lib/helper/constants.dart
@@ -50,7 +50,6 @@ import '../services/udp_service.dart';
 import 'functions.dart';
 import 'toast_messages.dart';
 
-
 enum LastKnownState {
   inside,
   outside,
@@ -331,10 +330,18 @@ class Constants {
 
   String privacyPolicyUrl() => 'https://smartnode.in/privacy-policy/';
 
+  String baseUrl() => SharedPreferenceManager.isLocalUrl()
+      ? 'http://10.10.10.102:5000/api/${apiVersion[0]}'
+      : 'https://test.api.smartnode.in/api/${apiVersion[0]}';
 
+  // : 'https://dev.smartnode.in/api/${apiVersion[0]}';
+  // : 'https://api.smartnode.in/api/${apiVersion[0]}';
 
   String v2BaseUrl() => SharedPreferenceManager.isLocalUrl()
+      ? 'http://10.10.10.102:5000/api/${apiVersion[1]}'
+      : 'https://test.api.smartnode.in/api/${apiVersion[1]}';
 
+  // : 'https://api.smartnode.in/api/${apiVersion[0]}';
 
   Future<bool> checkTutorialStatus({required String checkString}) async {
     bool isCompleteTutorial = false;
@@ -2798,5 +2805,3 @@ class Constants {
     return signedUrl;
   }
 }
-String baseUrl() => SharedPreferenceManager.isLocalUrl() ? 'http://10.10.10.102:5000/api/${apiVersion[0]}': 'https://test.api.smartnode.in/api/${apiVersion[0]}';
-//build_run_number=120
